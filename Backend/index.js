2e95d62993fcc065b759304989454c8a7ff4dd9c
IPCSGLOBALHQKOCHI <ipcsdeveloper@gmail.com>
2024-12-23 17:12:47 +0530

    deployed in vercel

--- Backend/index.js
+++ Backend/index.js
@@ -87,6 +87,10 @@ app.post("/api/submitform", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});   
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
