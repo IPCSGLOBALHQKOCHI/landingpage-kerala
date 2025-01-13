81debbe7d18cde7a91eccfa1bfe56f53adbc700a
IPCSGLOBALHQKOCHI <ipcsdeveloper@gmail.com>
2025-01-13 11:48:35 +0530

    instagam links changed

--- Frontend/src/Slides/ConstantSlides.jsx
+++ Frontend/src/Slides/ConstantSlides.jsx
@@ -4,131 +4,127 @@
 // import image4 from "../assets/images/ashik.png";
 // import image5 from "../assets/images/vyshna.png";
 // import image6 from "../assets/images/athulya.png"
-const image1 ="https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/Ruksana.png"
-const image2="https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/sinu.png"
-const image3="https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/sinan.png"
-const image4="https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/ashik.png"
-const image5="https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/vyshna.png"
-const image6="https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/athulya.png"
+const image1 =
+  "https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/Ruksana.png";
+const image2 =
+  "https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/sinu.png";
+const image3 =
+  "https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/sinan.png";
+const image4 =
+  "https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/ashik.png";
+const image5 =
+  "https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/vyshna.png";
+const image6 =
+  "https://campaigns.ipcsglobal.com/wp-content/uploads/2024/12/athulya.png";
 
- export const slides = [
-    {
-      name: "Ruksana ",
-      Position:
-        "Digital Marketing Executive",
-      imageUrl: image1,
-    },
-    {
-      name: "Sinu ",
-      Position:
-        "Digital Marketing Executive",
-      imageUrl: image2,
-    },
-    {
-      name: "Sinan ",
-      Position:
-        "Digital Marketing Executive",
-      imageUrl: image3,
-    },
-    {
-      name: "Ashik ",
-      Position:
-        "Digital Marketing Executive",
-      imageUrl: image4,
-    },
-    {
-      name: "Vyshna",
-      Position:
-        "Digital Marketing Executive",
-      imageUrl: image5,
-    },
-    {
-      name: "Athulya",
-      Position:
-        "Digital Marketing Executive",
-      imageUrl: image6,
-    }
-  ];
+export const slides = [
+  {
+    name: "Ruksana ",
+    Position: "Digital Marketing Executive",
+    imageUrl: image1,
+  },
+  {
+    name: "Sinu ",
+    Position: "Digital Marketing Executive",
+    imageUrl: image2,
+  },
+  {
+    name: "Sinan ",
+    Position: "Digital Marketing Executive",
+    imageUrl: image3,
+  },
+  {
+    name: "Ashik ",
+    Position: "Digital Marketing Executive",
+    imageUrl: image4,
+  },
+  {
+    name: "Vyshna",
+    Position: "Digital Marketing Executive",
+    imageUrl: image5,
+  },
+  {
+    name: "Athulya",
+    Position: "Digital Marketing Executive",
+    imageUrl: image6,
+  },
+];
 
-   export const slides2 = [
-    {
-      name: "Ruksana ",
-      Position:
-        "D M Executive",
-      imageUrl: image1,
-    },
-    {
-      name: "Sinu ",
-      Position:
-        "D M Executive",
-      imageUrl: image2,
-    },
-    {
-      name: "Sinan ",
-      Position:
-        "D M Executive",
-      imageUrl: image3,
-    },
-    {
-      name: "Ashik ",
-      Position:
-        "D M Executive",
-      imageUrl: image4,
-    },
-    {
-      name: "Vyshna",
-      Position:
-        "D M Executive",
-      imageUrl: image5,
-    },
-    {
-      name: "Athulya",
-      Position:
-        "D M Executive",
-      imageUrl: image6,
-    }
-  ];
+export const slides2 = [
+  {
+    name: "Ruksana ",
+    Position: "D M Executive",
+    imageUrl: image1,
+  },
+  {
+    name: "Sinu ",
+    Position: "D M Executive",
+    imageUrl: image2,
+  },
+  {
+    name: "Sinan ",
+    Position: "D M Executive",
+    imageUrl: image3,
+  },
+  {
+    name: "Ashik ",
+    Position: "D M Executive",
+    imageUrl: image4,
+  },
+  {
+    name: "Vyshna",
+    Position: "D M Executive",
+    imageUrl: image5,
+  },
+  {
+    name: "Athulya",
+    Position: "D M Executive",
+    imageUrl: image6,
+  },
+];
 
-  export const videoSlides = [
-    {
-      video: "https://www.instagram.com/reel/DCB3QsjCr-O/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DC7AH-azDgs/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DByZfw_CSyP/embed",
-    },
-    {
-      video: "https://www.instagram.com/p/DB8X-NQCIJx/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DCWf2dMCS9Y/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DCB3QsjCr-O/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DC7AH-azDgs/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DByZfw_CSyP/embed",
-    },
-    {
-      video: "https://www.instagram.com/p/DB8X-NQCIJx/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DCWf2dMCS9Y/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DByZfw_CSyP/embed",
-    },
-    {
-      video: "https://www.instagram.com/p/DB8X-NQCIJx/embed",
-    },
-    {
-      video: "https://www.instagram.com/reel/DCWf2dMCS9Y/embed",
-    },
-  ];
-
+export const videoSlides = [
+  {
+    video: "https://www.instagram.com/reel/DCYsanBPJw3/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCYtAp7J_hf/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCYuQqHKRIk/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DC_nq7MpjgX/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DEfDE8fPqmA/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DC7AH-azDgs/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCjH4emJLpw/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCYsanBPJw3/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCYtAp7J_hf/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCYuQqHKRIk/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DC_nq7MpjgX/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DEfDE8fPqmA/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DC7AH-azDgs/embed",
+  },
+  {
+    video: "https://www.instagram.com/reel/DCjH4emJLpw/embed",
+  }
+];
 // export default slides
